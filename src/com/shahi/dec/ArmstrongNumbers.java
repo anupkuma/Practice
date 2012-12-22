8461dc222fca4da84d2fc5bdc50aad160343f207
anupkuma <anupkuma@buffalo.edu>
2012-12-22 02:54:18 -0500

    Update src/com/shahi/dec/ArmstrongNumbers.java

--- src/com/shahi/dec/ArmstrongNumbers.java
+++ src/com/shahi/dec/ArmstrongNumbers.java
@@ -1,9 +1,17 @@
 package com.shahi.dec;
 
 public class ArmstrongNumbers {
-	public static void main(String[] args) {
-		for(int i=0;i<1000;i++){
-			
-		}
-	}
+ public boolean isArmstrong(int number){
+	 int num = number;
+	  int sum=0;
+	  while(num!=0){
+		 num = number%10;
+		 sum = sum + (num * num * num) ;
+		 num = num / 10;
+	  }
+	  if(sum==number){
+		  return true;
+	  }
+	  return false;
+  }
 }
